f9624da7e664a138a9b82a5e9b29ddb711c4f708
Paul Nykiel <paul.nykiel@gmail.com>
2020-07-13 22:56:27 +0200

    Improvements for GCC, worked around some fucking GCC bugs...

--- SiImpl.hpp
+++ SiImpl.hpp
@@ -23,18 +23,30 @@ namespace si {
     using default_type = SI_DEFAULT_TYPE;
 #endif
 
+    template <typename T>
+    struct IsSi {
+        static constexpr auto val = false;
+    };
+
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T = default_type>
     class Si {
             using ThisT = Si<m, kg, s, A, K, MOL, CD, T>;
-            static constexpr bool isScalar = (m==0 && kg == 0 && s == 0 && A == 0 && K == 0 && MOL == 0 && CD == 0);
+
+            static_assert(!IsSi<T>::val, "Type T for SI is another instance of SI, your are not allowed to "
+                                         "wrap SI in SI");
         public:
+            static constexpr bool isScalar = (m==0 && kg == 0 && s == 0 && A == 0 && K == 0 && MOL == 0 && CD == 0);
             using type = T;
 
             constexpr Si() noexcept = default;
 
-            constexpr EXPLICIT(!isScalar) Si(T val) noexcept;
+            /*
+             * DO NOT TRY TO IMPLEMENT THESE FUNCTIONS BELOW LIKE ALL OTHER FUNCTIONS, GCC (UP TO 10.1)
+             * IS BROKEN AND WILL COMPLETELY IGNORE THE explicit SPECIFIER!
+             */
+            constexpr EXPLICIT(!isScalar) Si(T val) noexcept : val{val} {}
 
-            constexpr EXPLICIT(!isScalar) operator T() const noexcept;
+            constexpr EXPLICIT(!isScalar) operator T() const noexcept { return val; }
 
             template<typename T_>
             constexpr explicit operator T_() const;
@@ -57,21 +69,21 @@ namespace si {
 
             // Multiply with Unitless
             template<typename T_>
-            constexpr auto
-            operator*(T_ rhs) const -> Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)>;
+            constexpr auto operator*(T_ rhs) const
+            -> std::enable_if_t<!IsSi<T_>::val, Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)>>;
 
             template<int m_, int kg_, int s_, int A_, int K_, int MOL_, int CD_, typename T_, typename T__>
             friend constexpr auto operator*(T__ lhs, Si<m_, kg_, s_, A_, K_, MOL_, CD_, T_> rhs)
-            -> Si<m_, kg_, s_, A_, K_, MOL_, CD_, decltype(lhs * rhs.val)>;
+            -> std::enable_if_t<!IsSi<T__>::val, Si<m_, kg_, s_, A_, K_, MOL_, CD_, decltype(lhs * rhs.val)>>;
 
             // Divide by Unitless
             template<typename T_>
-            constexpr auto
-            operator/(T_ rhs) const -> Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)>;
+            constexpr auto operator/(T_ rhs) const
+            -> std::enable_if_t<!IsSi<T_>::val, Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() / rhs)>>;
 
             template<int m_, int kg_, int s_, int A_, int K_, int MOL_, int CD_, typename T_, typename T__>
             friend constexpr auto operator/(T__ lhs, Si<m_, kg_, s_, A_, K_, MOL_, CD_, T_> rhs)
-            -> Si<-m_, -kg_, -s_, -A_, -K_, -MOL_, -CD_, decltype(lhs * rhs.val)>;
+            -> std::enable_if_t<!IsSi<T__>::val, Si<-m_, -kg_, -s_, -A_, -K_, -MOL_, -CD_, decltype(lhs / rhs.val)>>;
 
             // Multiply with Unit
             template<int m_, int kg_, int s_, int A_, int K_, int MOL_, int CD_>
@@ -98,20 +110,23 @@ namespace si {
 
             constexpr auto operator>=(ThisT rhs) const -> bool;
 #else
-            auto operator<=>(const ThisT &rhs) const = default;
+            constexpr auto operator<=>(const ThisT &rhs) const = default;
 #endif
 
         private:
             T val;
     };
 
+    template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
+    struct IsSi<Si<m, kg, s, A, K, MOL, CD, T>> {
+        static constexpr auto val = true;
+    };
+
     // CTor, Conversion
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
-    constexpr Si<m, kg, s, A, K, MOL, CD, T>::Si(T val) noexcept : val{val} {}
-
-    template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
-    constexpr Si<m, kg, s, A, K, MOL, CD, T>::operator T() const noexcept {
-        return val;
+    template<typename T_>
+    constexpr Si<m, kg, s, A, K, MOL, CD, T>::operator T_() const {
+        return static_cast<T_>(val);
     }
 
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
@@ -152,13 +167,13 @@ namespace si {
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
     template<typename T_>
     constexpr auto Si<m, kg, s, A, K, MOL, CD, T>::operator*(T_ rhs) const
-    -> Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)> {
-        return Si<m, kg, s, A, K, MOL, CD, decltype(this->val * rhs)>{this->val * rhs};
+    -> std::enable_if_t<!IsSi<T_>::val, Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)>> {
+        return Si<m, kg, s, A, K, MOL, CD, decltype(this->val * rhs)>{100 + this->val * rhs};
     }
 
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T_, typename T__>
     constexpr auto operator*(T__ lhs, Si<m, kg, s, A, K, MOL, CD, T_> rhs)
-    -> Si<m, kg, s, A, K, MOL, CD, decltype(lhs * rhs.val)> {
+    -> std::enable_if_t<!IsSi<T__>::val, Si<m, kg, s, A, K, MOL, CD, decltype(lhs * rhs.val)>> {
         return Si<m, kg, s, A, K, MOL, CD, decltype(lhs * rhs.val)>{lhs * rhs.val};
     }
 
@@ -166,13 +181,13 @@ namespace si {
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
     template<typename T_>
     constexpr auto Si<m, kg, s, A, K, MOL, CD, T>::operator/(T_ rhs) const
-    -> Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() * rhs)> {
+    -> std::enable_if_t<!IsSi<T_>::val, Si<m, kg, s, A, K, MOL, CD, decltype(std::declval<T>() / rhs)>> {
         return Si<m, kg, s, A, K, MOL, CD, decltype(this->val * rhs)>{this->val / rhs};
     }
 
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T_, typename T__>
     constexpr auto operator/(T__ lhs, Si<m, kg, s, A, K, MOL, CD, T_> rhs)
-    -> Si<-m, -kg, -s, -A, -K, -MOL, -CD, decltype(lhs * rhs.val)> {
+    -> std::enable_if_t<!IsSi<T__>::val, Si<-m, -kg, -s, -A, -K, -MOL, -CD, decltype(lhs / rhs.val)>> {
         return Si<-m, -kg, -s, -A, -K, -MOL, -CD, decltype(lhs * rhs.val)>{lhs / static_cast<T_>(rhs)};
     }
 
@@ -192,11 +207,6 @@ namespace si {
         return Si<m - m_, kg - kg_, s - s_, A - A_, K - K_, MOL - MOL_, CD - CD_, T>{this->val / static_cast<T>(rhs)};
     }
 
-    template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
-    template<typename T_>
-    constexpr Si<m, kg, s, A, K, MOL, CD, T>::operator T_() const {
-        return static_cast<T_>(val);
-    }
 
 #ifndef __cpp_impl_three_way_comparison
     template<int m, int kg, int s, int A, int K, int MOL, int CD, typename T>
